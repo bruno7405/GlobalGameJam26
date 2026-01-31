e19ca015260cf51ee4acb5d7532169ec803fbfa6
Tetratoric <cody.summerson@gmail.com>
2026-01-31 17:03:44 -0600

    Revert "state machine singleton"
    This reverts commit 8313b78c1735c447d91d876833382f34bf60b79d.

--- My project/Assets/General/State Machine/StateMachineManager.cs
+++ My project/Assets/General/State Machine/StateMachineManager.cs
@@ -2,18 +2,7 @@ using UnityEngine;
 
 public class StateMachineManager : MonoBehaviour
 {
-    public StateMachineManager Instance;
     public State currentState;
-
-    private void Awake()
-    {
-        if (Instance != null)
-        {
-            Destroy(gameObject);
-            return;
-        }
-        Instance = this;
-    }
 
     private void Start()
     {
