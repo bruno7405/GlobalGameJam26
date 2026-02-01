34776a43142f2c3f91b7eed3e15d18a118e3f7e6
Tetratoric <cody.summerson@gmail.com>
2026-02-01 15:50:21 -0600

    sounds

--- My project/Assets/General/State Machine/Game States/EndingState.cs
+++ My project/Assets/General/State Machine/Game States/EndingState.cs
@@ -6,8 +6,8 @@ public class EndingState : State
 {
     public Dialogue endingDialogue;
     public float fadeToBlackTime = 1;
-    public string gunshotSound;
-    public float gunSoundTime = 3;
+    public string gunshotSound = "silver_shoot";
+    public float gunSoundTime = 5;
 
     public override void GoToNextState()
     {
